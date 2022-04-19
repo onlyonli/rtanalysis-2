054055b6a03c869ad820fcfe7a630d93edfb59ea
onlyonli <104002027+onlyonli@users.noreply.github.com>
2022-04-19 17:19:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import time
 import requests
 import streamlit as st
 import webbrowser
+from bokeh.models.widgets import Div
 
 st.header("What to.. Where to .. What type of ... ? ")
 
@@ -9,7 +10,12 @@ insight = 'https://share.streamlit.io/pgstorm148/tac-traffic-v1/main/tac_app.py'
 poc = 'https://github.com/pgstorm148/eda-traffic/tree/main/eda-traffic'
 
 if st.button('Insights'):
-    webbrowser.open_new_tab(insight)
+    js = "window.open('https://share.streamlit.io/pgstorm148/tac-traffic-v1/main/tac_app.py')"  # New tab or window
+    js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)
+    
 
 if st.button('EDA - traffic'):
     webbrowser.open_new_tab(poc)
